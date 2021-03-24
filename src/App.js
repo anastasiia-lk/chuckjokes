9bb0f56b57cfdad901dd3eaf1b33bf0eb318c621
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2021-03-24 19:18:47 +0300

    Added Joke component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,38 @@ import { CssBaseline, Container } from '@material-ui/core';
 //Components
 import Joke from './components/Joke';
 
-const API_URL = `http://api.icndb.com/jokes/random/3`;
+const API_URL_jokes = `http://api.icndb.com/jokes/random/3`;
+const API_URL_categories = `http://api.icndb.com/categories`;
 
 function App() {
-  const [jokes, setJokes] = useState([]);
+  // const [jokes, setJokes] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(API_URL_jokes)
+  //     .then((res) => res.json())
+  //     .then((data) => setJokes(data.value));
+  //   console.log(jokes);
+  // }, []);
 
   useEffect(() => {
-    fetch(API_URL)
+    fetch(API_URL_categories)
       .then((res) => res.json())
-      .then((data) => setJokes(data.value));
-    console.log(jokes);
+      .then((res) => {
+        // setCategories(data.value);
+        console.log(res);
+        setCategories(res.value);
+      });
   }, []);
 
   return (
     <div className="App">
-      {jokes.map((joke) => {
+      {/* {jokes.map((joke) => {
         // return <p>{joke.joke}</p>;
         return <Joke joke={joke.joke} />;
+      })} */}
+      {categories.map((category) => {
+        return <Joke category={category} />;
       })}
     </div>
   );
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   CssBaseline,
   Container,
@@ -6,14 +6,30 @@ import {
   Paper,
   Box,
   Grid,
+  Button,
 } from '@material-ui/core';
 import { borders } from '@material-ui/system';
 
-const Joke = ({ joke }) => {
+export default function Joke({ category }) {
+  const API_URL_categories = `http://api.icndb.com/jokes/random?limitTo=[${category}]`;
+
+  const [joke, setJoke] = useState('');
+  const [buttonText, setButtonText] = useState('Open for more');
+
+  useEffect(() => {
+    fetch(API_URL_categories)
+      .then((res) => res.json())
+      // .then((data) => setJoke(data.value));
+      .then((res) => {
+        console.log(res);
+        setJoke(res.value.joke);
+      });
+  }, []);
+
   return (
     <div>
       <Box
-        width="500px"
+        width="700px"
         display="flex"
         justifyContent="center"
         // p={1}
@@ -22,12 +38,13 @@ const Joke = ({ joke }) => {
         border={1}
         borderColor="grey.500"
       >
-        <Box m={5}>Chuck Norris</Box>
-        <Box m={5}>Category</Box>
-        <Box m={5}>Button</Box>
+        <Box m={5}>CHUCK NORRIS JOKE</Box>
+        <Box m={5}>Category: {category}</Box>
+        <Box m={5}>
+          <Button variant="contained"> {buttonText} </Button>
+        </Box>
+        <Box m={5}>{joke}</Box>
       </Box>
     </div>
   );
-};
-
-export default Joke;
+}
